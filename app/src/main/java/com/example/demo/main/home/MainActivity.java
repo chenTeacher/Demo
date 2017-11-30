9f710857b1018f3ccd227ced62d61eeb509f2811
ChenTeacher <343542102@qq.com>
2017-11-30 20:51:53 +0800

    ...

--- app/src/main/java/com/example/demo/main/home/MainActivity.java
+++ app/src/main/java/com/example/demo/main/home/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends FragmentActivity  implements TabHost.OnTabChan
     @Override
     public void onTabChanged(String tabId) {
         Log.i("MainActivity","onTabChanged:tabId"+tabId);
+//        cs
     }
 
 }
